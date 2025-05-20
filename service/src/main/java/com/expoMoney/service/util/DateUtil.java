de1b1687c406c07b1649483ee2c6e4e9314569dc
Desenvolvedor <desenvolvedor@desenvolvedor.com>
2025-05-20 13:44:47 -0300

    validando data inicio em calculo de dias

--- service/src/main/java/com/expoMoney/service/util/DateUtil.java
+++ service/src/main/java/com/expoMoney/service/util/DateUtil.java
@@ -6,7 +6,7 @@ import java.time.Period;
 public class DateUtil {
 
     public static int CalculateTheDifferenceInDaysBetweenTwoDates(LocalDate dateStart, LocalDate dateEnd){
-        Period period = Period.between(dateStart, dateEnd);
+        Period period = Period.between(dateStart != null ? dateStart : LocalDate.now() , dateEnd);
         return period.getDays();
     }
 }
